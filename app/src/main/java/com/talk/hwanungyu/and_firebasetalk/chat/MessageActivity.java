c56cd54dfb2f5679b8a6eb1092144c981726742b
hwanungyu <cacao1562@naver.com>
2017-10-03 01:11:55 +0900

    1003

--- app/src/main/java/com/talk/hwanungyu/and_firebasetalk/chat/MessageActivity.java
+++ app/src/main/java/com/talk/hwanungyu/and_firebasetalk/chat/MessageActivity.java
@@ -7,7 +7,10 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.talk.hwanungyu.and_firebasetalk.R;
 import com.talk.hwanungyu.and_firebasetalk.model.ChatModel;
 
@@ -17,12 +20,16 @@ public class MessageActivity extends AppCompatActivity {
     private Button button;
     private EditText editText;
 
+    private String uid;
+    private String chatRoomUid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
-        destinationUid = getIntent().getStringExtra("destinationUid");
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();  //채팅 요구 하는 아이디 로그인된 uid
+        destinationUid = getIntent().getStringExtra("destinationUid"); //채팅 당하는 아이디
         button = (Button)findViewById(R.id.messageActivity_button);
         editText = (EditText)findViewById(R.id.messageActivity_edittext);
 
@@ -30,12 +37,43 @@ public class MessageActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 ChatModel chatModel = new ChatModel();
-                chatModel.uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                chatModel.destinationUid = destinationUid;
-                                                                                //push가 있어야 채팅방 이름이 생김
-                FirebaseDatabase.getInstance().getReference().child("chatrooms").push().setValue(chatModel);
+                chatModel.users.put(uid,true);
+                chatModel.users.put(destinationUid,true);
+
+                if(chatRoomUid == null) {
+                                                                                    //push가 있어야 채팅방 이름이 생김
+                    FirebaseDatabase.getInstance().getReference().child("chatrooms").push().setValue(chatModel);
+                } else {
+                    ChatModel.Comment comment = new ChatModel.Comment();
+                    comment.uid = uid;
+                    comment.message = editText.getText().toString();
+
+
+                    FirebaseDatabase.getInstance().getReference().child("chatrooms").child(chatRoomUid).child("comments").push().setValue(comment);
+                }
+
             }
         });
 
+        checkChatRoom();
+    }
+
+    void checkChatRoom() {
+        FirebaseDatabase.getInstance().getReference().child("chatrooms").orderByChild("users/"+uid).equalTo(true).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot item : dataSnapshot.getChildren()) {
+                    ChatModel chatModel = item.getValue(ChatModel.class);
+                    if(chatModel.users.containsKey(destinationUid)) {
+                        chatRoomUid = item.getKey();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/talk/hwanungyu/and_firebasetalk/model/ChatModel.java
+++ app/src/main/java/com/talk/hwanungyu/and_firebasetalk/model/ChatModel.java
@@ -1,11 +1,25 @@
 package com.talk.hwanungyu.and_firebasetalk.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by hwanungyu on 2017. 9. 25..
  */
 
 public class ChatModel {
-    public String uid;
-    public String destinationUid;
+    //public String uid;
+    //public String destinationUid;
+
+    public Map<String,Boolean> users = new HashMap<>(); //채팅방 유저들
+    public Map<String,Comment> comments = new HashMap<>(); //채팅방의 대화내용
+
+    public static class Comment {
+        public String uid;
+        public String message;
+
+
+    }
+
 
 }
