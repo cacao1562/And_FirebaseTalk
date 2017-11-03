806c3d532bc08b4349740d221dd5eb31a581f106
hwanungyu <cacao1562@naver.com>
2017-11-03 14:32:16 +0900

    1103

--- app/src/main/java/com/talk/hwanungyu/and_firebasetalk/MainActivity.java
+++ app/src/main/java/com/talk/hwanungyu/and_firebasetalk/MainActivity.java
@@ -1,8 +1,12 @@
 package com.talk.hwanungyu.and_firebasetalk;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.talk.hwanungyu.and_firebasetalk.fragment.ChatFragment;
 import com.talk.hwanungyu.and_firebasetalk.fragment.Peoplefragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.mainActivity_bottomnavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_people:
+                        getFragmentManager().beginTransaction().replace(R.id.mainActivity_framelayout, new Peoplefragment()).commit();
+                        return true;
 
-        getFragmentManager().beginTransaction().replace(R.id.mainActivity_framelayout, new Peoplefragment()).commit();
+                    case R.id.action_chat:
+                        getFragmentManager().beginTransaction().replace(R.id.mainActivity_framelayout, new ChatFragment()).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/talk/hwanungyu/and_firebasetalk/fragment/ChatFragment.java
@@ -0,0 +1,86 @@
+package com.talk.hwanungyu.and_firebasetalk.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.talk.hwanungyu.and_firebasetalk.R;
+import com.talk.hwanungyu.and_firebasetalk.model.ChatModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hwanungyu on 2017. 11. 3..
+ */
+
+public class ChatFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chat,container,false);
+        RecyclerView recyclerView = view.findViewById(R.id.chatfragment_recyclerview);
+
+        recyclerView.setAdapter(new ChatRecyclerViewAdapter());
+        recyclerView.setLayoutManager(new LinearLayoutManager(inflater.getContext()));
+        return view;
+    }
+
+    class ChatRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        private List<ChatModel> chatModels = new ArrayList<>();
+        private String uid;
+        public ChatRecyclerViewAdapter() {
+            uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+            FirebaseDatabase.getInstance().getReference().child("chatrooms").orderByChild("users/"+uid).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    chatModels.clear();
+                    for (DataSnapshot item : dataSnapshot.getChildren()){
+                        chatModels.add(item.getValue(ChatModel.class));
+                    }
+                    notifyDataSetChanged(); //새로고침
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_chat,parent,false);
+
+            return new CustomViewHoler(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return chatModels.size();
+        }
+
+        private class CustomViewHoler extends RecyclerView.ViewHolder {
+            public CustomViewHoler(View view) {
+                super(view);
+            }
+        }
+    }
+}
