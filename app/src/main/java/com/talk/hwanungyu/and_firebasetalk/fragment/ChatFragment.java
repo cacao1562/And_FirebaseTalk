9a9486e1754d802a3d288a5750fb8277e43fc2b3
hwanungyu <cacao1562@naver.com>
2017-11-07 12:52:57 +0900

    1107

--- app/src/main/java/com/talk/hwanungyu/and_firebasetalk/fragment/ChatFragment.java
+++ app/src/main/java/com/talk/hwanungyu/and_firebasetalk/fragment/ChatFragment.java
@@ -8,7 +8,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,9 +20,15 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.talk.hwanungyu.and_firebasetalk.R;
 import com.talk.hwanungyu.and_firebasetalk.model.ChatModel;
+import com.talk.hwanungyu.and_firebasetalk.model.UserModel;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by hwanungyu on 2017. 11. 3..
@@ -70,6 +80,38 @@ public class ChatFragment extends Fragment {
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
+            final CustomViewHoler customViewHoler = (CustomViewHoler)holder;
+            String destinationUid = null;
+
+            //챗방에 있는 유저 다 체크
+            for (String user: chatModels.get(position).users.keySet()) {
+                if (!user.equals(uid)) {
+                    destinationUid = user;
+                }
+            }
+            FirebaseDatabase.getInstance().getReference().child("users").child(destinationUid).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    UserModel userModel = dataSnapshot.getValue(UserModel.class);
+                    Glide.with(customViewHoler.itemView.getContext())
+                            .load(userModel.profileImageUrl)
+                            .apply(new RequestOptions().circleCrop())
+                            .into(customViewHoler.imageView);
+                    customViewHoler.textView_title.setText(userModel.name);
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+            //메세지를 내림차순으로 정렬 후 마지막 메세지의 키값을 가져옴
+            Map<String,ChatModel.Comment> commentMap = new TreeMap<>(Collections.reverseOrder());
+            commentMap.putAll(chatModels.get(position).comments);
+            String lastMessageKey = (String) commentMap.keySet().toArray()[0];
+            customViewHoler.textView_last_message.setText(chatModels.get(position).comments.get(lastMessageKey).message);
         }
 
         @Override
@@ -78,8 +120,15 @@ public class ChatFragment extends Fragment {
         }
 
         private class CustomViewHoler extends RecyclerView.ViewHolder {
+            public ImageView imageView;
+            public TextView textView_title;
+            public TextView textView_last_message;
             public CustomViewHoler(View view) {
+
                 super(view);
+                imageView = view.findViewById(R.id.chatitem_imageview);
+                textView_title = view.findViewById(R.id.chatitem_textview_title);
+                textView_last_message = view.findViewById(R.id.chatitem_textview_lastMessage);
             }
         }
     }
